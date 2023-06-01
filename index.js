43e0c071eb5adddd0039b3a13beaa333b75efb63
Léona Ardoin <126014224+leonaardoin@users.noreply.github.com>
2023-06-01 12:00:56 -0400

    Updated

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const dotenv = require('dotenv').config()
 const mongoose = require("mongoose")
 const authController = require('./controllers/authController')
 const productController = require('./controllers/productController')
 const uploadController = require('./controllers/uploadController')
 const app = express()
+const PORT = 3000
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
+
+app.use(express.urlencoded({extended:false}));
 
 // connect our db
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGO_URL, () => console.log('DB is successfully connected'))
+// mongoose.set('strictQuery', false)
+// mongoose.connect(process.env.MONGO_URL, () => console.log('DB is successfully connected'))
 
 // routes & middlewares
 // those two middlewares make req.body accessible, otherwise it would be undefined
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
 app.use('/images', express.static('public/images'))
 app.use('/auth', authController)
 app.use('/product', productController)
@@ -24,5 +31,4 @@ app.use('/upload', uploadController)
 // start our server
 app.listen(process.env.PORT, () => console.log('Server has been started'))
 
-// server is on port 5000, client is on port 3000,
 // we are going to get a cors ERROR!!, but cors() removes that's error
